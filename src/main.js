1e9b51196cc099c5369f05c94adeb0f70e8e9f5a
wz <1528740766@qq.com>
2022-06-16 23:16:18 +0800

    第二天

--- src/main.js
+++ src/main.js
@@ -8,7 +8,13 @@ import 'element-ui/lib/theme-chalk/index.css';
 import './assets/fonts/iconfont.css';
 
 import axios from 'axios';
+
+// 配置请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/';
+axios.interceptors.request.use(config => {
+  config.headers.Authorization = window.sessionStorage.getItem('token');
+  return config;
+})
 Vue.prototype.$http = axios;
 // 导入全局样式biao
 import './assets/css/global.css'
