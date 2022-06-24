11666335a5849286689dd87c35cd445fe784f9cb
wz <1528740766@qq.com>
2022-06-24 12:07:52 +0800

    第三天

--- src/components/user/users.vue
+++ src/components/user/users.vue
@@ -28,21 +28,21 @@
                 <el-table-column label="角色" prop="role_name"></el-table-column>
                 <el-table-column label="状态">
                     <template slot-scope="scope">
-                        <el-switch v-model="scope.row.mg_state" @change="userStateChanged(scope.row.id)">
+                        <el-switch v-model="scope.row.mg_state" @change="userStateChanged(scope.row)">
                         </el-switch>
                     </template>
                 </el-table-column>
-                <el-table-column label="操作">
+                <el-table-column label="操作" width="180px">
                     <template slot-scope="scope">
                         <!-- 修改按钮 -->
                         <el-button type="primary" icon="el-icon-edit" size="mini" @click="showEditDialog(scope.row.id)">
                         </el-button>
                         <!-- 删除按钮 -->
-                        <el-button type="danger" icon="el-icon-delete" size="mini"
-                            @click="removeUserById(scope.row.id)"></el-button>
+                        <el-button type="danger" icon="el-icon-delete" size="mini" @click="removeUserById(scope.row.id)"></el-button>
                         <!-- 分配角按钮 -->
-                        <el-tooltip effect="dark" content="分配角色" placement="top">
-                            <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+                        <el-tooltip effect="dark" content="分配角色" placement="top" :enterable="false">
+                            <el-button type="warning" icon="el-icon-setting" size="mini" @click="setRole(scope.row)">
+                            </el-button>
                         </el-tooltip>
                     </template>
                 </el-table-column>
@@ -78,21 +78,38 @@
 
 
         <!-- 修改用户的对话框 -->
-        <el-dialog title="修改用户" :visible.sync="eidetDialogVisible" width="50%" @click="editDialogClosed">
-            <el-form :model="editForm" ref="editFormRef" :rules="editFormRules" label-width="70px">
+        <el-dialog title="修改用户" :visible.sync="editDialogVisible" width="50%" @close="editDialogClosed">
+            <el-form :model="editForm" :rules="editFormRules" ref="editFormRef" label-width="70px">
                 <el-form-item label="用户名">
                     <el-input v-model="editForm.username" disabled></el-input>
                 </el-form-item>
                 <el-form-item label="邮箱" prop="email">
-                    <el-input v-model="editForm.email" ></el-input>
+                    <el-input v-model="editForm.email"></el-input>
                 </el-form-item>
-                <el-form-item label="手机号" prop="mobile">
-                    <el-input v-model="editForm.mobile" ></el-input>
+                <el-form-item label="手机" prop="mobile">
+                    <el-input v-model="editForm.mobile"></el-input>
                 </el-form-item>
             </el-form>
             <span slot="footer" class="dialog-footer">
-                <el-button @click="eidetDialogVisible = false">取 消</el-button>
+                <el-button @click="editDialogVisible = false">取 消</el-button>
                 <el-button type="primary" @click="editUserInfo">确 定</el-button>
+            </span>
+        </el-dialog>
+        <!-- 分配角色的对话框 -->
+        <el-dialog title="分配角色" :visible.sync="setRoleDialogVisible" width="50%" @close="setRoleDialogClosed">
+            <div>
+                <p>当前的用户：{{userInfo.username}}</p>
+                <p>当前的角色：{{userInfo.role_name}}</p>
+                <p>分配新角色：
+                    <el-select v-model="selectedRoleId" placeholder="请选择">
+                        <el-option v-for="item in rolesList" :key="item.id" :label="item.roleName" :value="item.id">
+                        </el-option>
+                    </el-select>
+                </p>
+            </div>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="setRoleDialogVisible = false">取 消</el-button>
+                <el-button type="primary" @click="saveRoleInfo">确 定</el-button>
             </span>
         </el-dialog>
     </div>
@@ -101,7 +118,6 @@
 <script>
 export default {
     data() {
-
         // 验证邮箱的规则
         var checkEmail = (rule, value, cb) => {
             // 验证邮箱的正则表达式
@@ -163,7 +179,7 @@ export default {
                     { validator: checkMobile, trigger: 'blur' }
                 ]
             },
-            eidetDialogVisible: false,
+            editDialogVisible: false,
             editForm: {},
             editFormRules: {
                 email: [
@@ -174,8 +190,11 @@ export default {
                     { required: true, message: '请输入手机号', trigger: 'blur' },
                     { validator: checkMobile, trigger: 'blur' }
                 ]
-            }
-
+            },
+            setRoleDialogVisible: false,
+            userInfo: {},
+            rolesList: [],
+            selectedRoleId: ''
         }
     },
     created() {
@@ -230,32 +249,32 @@ export default {
             })
         },
         async showEditDialog(id) {
-            this.eidetDialogVisible = true
             const { data: res } = await this.$http.get('users/' + id)
             if (res.meta.status !== 200) {
-                return this.$message.error('获取用户信息失败！')
+                return this.$message.error('查询用户信息失败！')
             }
             this.editForm = res.data
-            this.eidetDialogVisible = true
+            this.editDialogVisible = true
         },
         editDialogClosed() {
             this.$refs.editFormRef.resetFields()
         },
-        editUserInfo(id) {
+        editUserInfo() {
             this.$refs.editFormRef.validate(async valid => {
                 if (!valid) return
-                const { data: res } = await this.$http.put('users/' + this.editForm.id,
+                const { data: res } = await this.$http.put(
+                    'users/' + this.editForm.id,
                     {
                         email: this.editForm.email,
                         mobile: this.editForm.mobile
                     })
                 if (res.meta.status !== 200) {
-                    this.$message.error('更新用户信息失败！')
+                   return this.$message.error('更新用户信息失败！')
                 }
-                this.eidetDialogVisible = false
+                this.editDialogVisible = false
                 this.getUserList()
                 this.$message.success('更新用户信息成功！')
-                
+
             })
         },
         async removeUserById(id) {
@@ -275,7 +294,38 @@ export default {
             }
             this.$message.success('删除用户成功！')
             this.getUserList()
+        },
+        async setRole(userInfo) {
+            this.userInfo = userInfo
+            const { data: res } = await this.$http.get('roles')
+            if (res.meta.status !== 200) {
+                return this.$message.error('获取角色列表失败')
+            }
+            this.rolesList = res.data
+            this.setRoleDialogVisible = true
+        },
+        async saveRoleInfo() {
+            if (!this.selectedRoleId) {
+                return this.$message.error('请选择要分配的角色！')
+            }
+            const { data: res } = await this.$http.put(
+                `users/${this.userInfo.id}/role`,
+            {
+                rid: this.selectedRoleId
+            }
+            )
+            if (res.meta.status !== 200) {
+                return this.$message.error('更新角色失败！')
+            }
+            this.$message.success('更新角色成功！')
+            this.getUserList()
+            this.setRoleDialogVisible = false
+        },
+        setRoleDialogClosed() {
+            this.selectedRoleId = ''
+            this.userInfo = {}
         }
+
     }
 }
 
--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,18 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+  // devServer: {
+  //   port: 8080,
+  //   open: true,
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://192.168.1.104:8888',
+  //       changeOrigin: true,
+  //       pathRewrite: {
+  //         '^/api': ''
+  //       }
+  //     }
+  //   }
+  // }
+
 })
